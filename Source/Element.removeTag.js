d2d1c6a0509917a77b48288afa01db48595f6554
Jaik Dean <jaik@studioskylab.com>
2011-01-11 13:51:54 +0000

    Changed nodeType testing to use typeOf()

--- Source/Element.removeTag.js
+++ Source/Element.removeTag.js
@@ -15,10 +15,14 @@ Element.implement({
 		var childNodes = this.childNodes;
 
 		for (var i = childNodes.length - 1; i >= 0; i--) {
-			if (childNodes[i].nodeType == document.TEXT_NODE) {
-				this.appendText(childNodes[i].nodeValue, 'after');
-			} else {
-				document.id(childNodes[i]).inject(this, 'after');
+			switch (typeOf(childNodes[i])) {
+				case 'textnode':
+				case 'whitespace':
+					this.appendText(childNodes[i].nodeValue, 'after');
+				break;
+				case 'element':
+					$(childNodes[i]).inject(this, 'after');
+				break;
 			}
 		}
 
